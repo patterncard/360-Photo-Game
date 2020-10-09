3386a3a487b0158760d16c7925ae63e9e66b0d5e
Patrycja Cader <patcad@o2.pl>
2020-10-09 23:10:07 +0200

    fixes

--- 360 Photo Game/Assets/Scripts/DestinationPinController.cs
+++ 360 Photo Game/Assets/Scripts/DestinationPinController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRStandardAssets.Utils;
 
-[RequireComponent(typeOf(VRInteractiveItem))]
+[RequireComponent(typeof(VRInteractiveItem))]
 public class DestinationPinController : MonoBehaviour
 {
     public string sceneName;
