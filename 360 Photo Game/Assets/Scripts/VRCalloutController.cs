bc12869fa0afebd5266e7de677c22ebe7014fa0c
Patrycja Cader <patcad@o2.pl>
2020-10-15 23:13:06 +0200

    callout controller

---
+++ 360 Photo Game/Assets/Scripts/VRCalloutController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+[RequireComponent(typeof(VRInteractiveItem))]
+public class VRCalloutController : MonoBehaviour
+{
+    public Canvas canvas;
+    public bool isInitiallyVisible = false;
+    public bool isHoverActivated = false;
+    public bool isClickActivated = false;
+    VRInteractiveItem vrInteractive;
+
+    void Awake()
+    {
+        vrInteractive = GetComponent<VRInteractiveItem>();
+    }
+
+    void OnEnable()
+    {
+        if(isHoverActivated)
+        {
+            vrInteractive.OnOver += ShowCanvas;
+            vrInteractive.OnOut += HideCanvas;
+        }
+    }
+
+    void OnDisable()
+    {
+
+    }
+
+    void ShowCanvas()
+    {
+        canvas.enabled = true;
+    }
+
+    void HideCanvas()
+    {
+        canvas.enabled = false;
+    }
+}
