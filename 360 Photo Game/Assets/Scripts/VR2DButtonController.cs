493ae0871a48cc57a0f156f9cb40e7ef051500a3
Patrycja Cader <patcad@o2.pl>
2020-10-19 10:30:50 +0200

    interactive button

---
+++ 360 Photo Game/Assets/Scripts/VR2DButtonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using VRStandardAssets.Utils;
+
+[RequireComponent(typeof(VRInteractiveItem))]
+public class VR2DButton : MonoBehaviour
+{
+    VRInteractiveItem VRInteractive;
+    Button button;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        VRInteractive = GetComponent<VRInteractiveItem>();
+        button = GetComponentInChildren<Button>();
+    }
+
+    // Update is called once per frame
+    void OnEnable()
+    {
+        
+    }
+         void OnDisable()
+    {
+        
+    }
+}
